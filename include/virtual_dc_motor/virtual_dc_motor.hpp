86a2c6aebaffbe9180b08213e5267284489b1898
igorosky <igor.zaworski2@gmail.com>
2024-02-18 00:34:18 +0100

    multiple motors

--- include/virtual_dc_motor/virtual_dc_motor.hpp
+++ include/virtual_dc_motor/virtual_dc_motor.hpp
@@ -6,11 +6,12 @@
 #include <std_msgs/Int8.h>
 #include <std_msgs/UInt16.h>
 #include <random>
+#include <string>
 
-class VirtualDCMotorNode
+class VirtualDCMotor
 {
 public:
-    VirtualDCMotorNode();
+    VirtualDCMotor(ros::NodeHandle&, std::shared_ptr<std::mt19937>, const std::string);
 
     /**
      * @brief Callback function invoked every time new steering signal is received
@@ -19,33 +20,26 @@ public:
      */
     void controlSignalCallback(const std_msgs::Int8::ConstPtr &msg);
 
-    /**
-     * @brief Main program loop resposible for virtual dc motor simulation
-     *
-     */
     void run();
 
     /**
      * @brief Function responsible for simulating non linear motor behavior
      *
-     * @param cs Input Control Signal (CS)
      * @return int16_t Resulting motor position
      */
-    int16_t simulateNonLinearMotorBehavior(int8_t cs);
+    void simulateNonLinearMotorBehavior();
 
     uint8_t generateRandomNumber();
 
 private:
-    ros::NodeHandle nh_;
     ros::Subscriber set_cs_sub_;
     ros::Publisher get_position_pub_;
 
-    ros::Rate loop_rate_;
     int8_t cs_goal_, cs_real_;
     int16_t position_;
-    int count_;
+    size_t count_;
 
-    std::mt19937 random_number_generator_ = std::mt19937(std::random_device()());
+    std::shared_ptr<std::mt19937> random_number_generator_;
     std::uniform_int_distribution<uint8_t> random_number_distribution_ = std::uniform_int_distribution<uint8_t>(0, 100);
 };
 
