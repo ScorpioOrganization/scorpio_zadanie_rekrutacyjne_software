099fee316d802ef20f5069cadd147dd0d513b206
Mariusz Rychlicki <mariuszrychlicki04@gmail.com>
2026-02-21 16:19:05 +0100

    Example node
    Signed-off-by: Mariusz Rychlicki <mariuszrychlicki04@gmail.com>

--- src/cube_detector_node.cpp
+++ src/cube_detector_node.cpp
@@ -1,25 +1,28 @@
 #include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/string.hpp"
 
+/**
+ * This is an example - you can remove/modify it and implement your own node.
+ */
 namespace cube_detector {
 class CubeDetectorNode : public rclcpp::Node {
 public:
   CubeDetectorNode(const rclcpp::NodeOptions& options)
-  : rclcpp::Node("cube_detector_node", options),
+  : rclcpp::Node("cube_detector", options),
     _publisher(this->create_publisher<std_msgs::msg::String>(
-      "~/echoed", rclcpp::QoS(1))),
+      "~/output", rclcpp::QoS(1))),
     _subscription(this->create_subscription<std_msgs::msg::String>(
-      "~/echo", rclcpp::QoS(1),
-      std::bind(&CubeDetectorNode::echo_callback, this, std::placeholders::_1))) { }
+      "~/input", rclcpp::QoS(1),
+      std::bind(&CubeDetectorNode::input_callback, this, std::placeholders::_1))) { }
 
 private:
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher;
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr _subscription;
 
-  void echo_callback(const std_msgs::msg::String::SharedPtr msg) {
+  void input_callback(const std_msgs::msg::String::SharedPtr msg) {
     RCLCPP_INFO(this->get_logger(), "Received: '%s'", msg->data.c_str());
     auto echoed_msg = std_msgs::msg::String();
-    echoed_msg.data = "Echo: " + msg->data;
+    echoed_msg.data = "Received: " + msg->data;
     _publisher->publish(echoed_msg);
   }
 };
